d5d386ae0eac8dd890ebfa8cb0b38851a1300abf
Sergey Vorontsov <aurum1997@gmail.com>
2024-04-13 17:46:26 +0300

    Highly configurable, compatible with other devices

--- deepcool-digital-info.py
+++ deepcool-digital-info.py
@@ -1,34 +1,155 @@
-from sys import platform
-import time
-# use package hidapi!
-import hid
+#!/bin/python3
 
-VENDOR_ID = 0x34d3
-PRODUCT_ID = 0x1100
+import sys
+import time
+import hid
+import psutil
+import argparse
+
+CUR_DEVICE = "CUSTOM"
+SENSOR = 'k10temp'
 INTERVAL = 1
 
+
+class DeviceInfo:
+    def __init__(self, vendor_id, product_id, simple_mode):
+        self.VENDOR_ID = vendor_id
+        self.PRODUCT_ID = product_id
+        self.SIMPLE_MODE = simple_mode
+
+
+DEVICES = {
+    "CH510": DeviceInfo(vendor_id=0x34d3, product_id=0x1100, simple_mode=True),
+    "AK620": DeviceInfo(vendor_id=0x3633, product_id=0x0002, simple_mode=False),
+    "AK400": DeviceInfo(vendor_id=0x3633, product_id=0x0001, simple_mode=False),
+    "CUSTOM": DeviceInfo(vendor_id=0x0, product_id=0x0000, simple_mode=False)
+}
+
+parser = argparse.ArgumentParser(
+    prog=sys.argv[0],
+    description='This project provides support for Deepcool Digital for Linux OS. The Python script '
+                'is tailored exclusively for the developer’s equipment; work on other devices is not '
+                'guaranteed, but you can always make your own changes. Use root access to access the '
+                'device')
+parser.add_argument('-d', '--device', choices=list(DEVICES.keys()), nargs='?', help='Select your device. If it is '
+                                                                                    'not in the list, then select '
+                                                                                    '“CUSTOM”',
+                    default=CUR_DEVICE)
+parser.add_argument('-i', '--interval', type=int, nargs='?', help='display refresh timing in seconds', default=INTERVAL)
+parser.add_argument('-s', '--sensor', default=SENSOR, nargs='?', type=str)
+parser.add_argument('-m', '--simple', action='store_true',
+                    help="specify if using simple data sending mode (if "
+                         "your device is on the list, don't worry about "
+                         "it)", default=None)
+parser.add_argument('-v', '--vendor', type=lambda x: int(x, 0), nargs='?',
+                    help="provide a specific VENDOR_ID (if your device is listed, "
+                         "don't worry about it)", default=None)
+parser.add_argument('-p', '--product', type=lambda x: int(x, 0), nargs='?',
+                    help="provide a specific PRODUCT_ID (if your device is listed, "
+                         "don't worry about it)", default=None)
+
+args = parser.parse_args()
+INTERVAL = args.interval
+SENSOR = args.sensor
+CUR_DEVICE = args.device
+if args.simple is not None:
+    DEVICES[CUR_DEVICE].SIMPLE_MODE = True
+if args.vendor is not None:
+    DEVICES[CUR_DEVICE].VENDOR_ID = args.vendor
+if args.product is not None:
+    DEVICES[CUR_DEVICE].PRODUCT_ID = args.product
+
+
+def get_bar_value(input_value):
+    return (input_value - 1) // 10 + 1
+
+
+def get_data(value=0, mode='util'):
+    if mode == 'simple':
+        simple_data = bytearray()
+        simple_data.extend(map(ord, f"_HLXDATA({value},{value},0,0,C)"))
+        return simple_data
+    base_data = [16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    numbers = [int(char) for char in str(value)]
+    base_data[2] = get_bar_value(value)
+    if mode == 'util':
+        base_data[1] = 76
+    elif mode == 'start':
+        base_data[1] = 170
+        return base_data
+    elif mode == 'temp':
+        base_data[1] = 19
+    if len(numbers) == 1:
+        base_data[5] = numbers[0]
+    elif len(numbers) == 2:
+        base_data[4] = numbers[0]
+        base_data[5] = numbers[1]
+    elif len(numbers) == 3:
+        base_data[3] = numbers[0]
+        base_data[4] = numbers[1]
+        base_data[5] = numbers[2]
+    elif len(numbers) == 4:
+        base_data[3] = numbers[0]
+        base_data[4] = numbers[1]
+        base_data[5] = numbers[2]
+        base_data[6] = numbers[3]
+    return base_data
+
+
+def get_cpu_temperature(label="CPU"):
+    sensors = psutil.sensors_temperatures()
+    for sensor_label, sensor_list in sensors.items():
+        for sensor in sensor_list:
+            if sensor.label == label:
+                return sensor.current
+    return 0
+
+
 def get_temperature():
-    if platform == "linux" or platform == "linux2":
-        #In this case, a method is called that returns the temperature of my processor.
-        import psutil
-        return psutil.sensors_temperatures()['k10temp'][0].current
+    try:
+        temp_sensor = round(psutil.sensors_temperatures()[SENSOR][0].current)
+    except KeyError:
+        temp_sensor = get_cpu_temperature()
+    if DEVICES[CUR_DEVICE].SIMPLE_MODE:
+        return get_data(value=temp_sensor, mode='simple')
     else:
-        #out fake temp
-        return 60
+        return get_data(value=temp_sensor, mode='temp')
+
+
+def get_utils():
+    utils_data = round(psutil.cpu_percent())
+    return get_data(value=utils_data, mode='util')
+
 
 try:
-    h = hid.device()
-    h.open(VENDOR_ID, PRODUCT_ID)
-
-    h.set_nonblocking(1)
-
+    hidDevice = hid.device()
+    hidDevice.open(DEVICES[CUR_DEVICE].VENDOR_ID, DEVICES[CUR_DEVICE].PRODUCT_ID)
+    hidDevice.set_nonblocking(1)
+    if not DEVICES[CUR_DEVICE].SIMPLE_MODE:
+        hidDevice.write(get_data(mode="start"))
     while True:
-        temp = get_temperature()
-        b = bytearray()
-        b.extend(map(ord, f"_HLXDATA({temp},{temp},0,0,C)"))
-        h.write(b)
-        time.sleep(INTERVAL)
+        if DEVICES[CUR_DEVICE].SIMPLE_MODE == "simple":
+            temp = get_temperature()
+            b = bytearray()
+            b.extend(map(ord, f"_HLXDATA({temp},{temp},0,0,C)"))
+            hidDevice.write(b)
+            time.sleep(INTERVAL)
+            continue
 
+        hidDevice.set_nonblocking(1)
+        temp = get_temperature()
+        hidDevice.write(temp)
+        time.sleep(INTERVAL)
+        utils = get_utils()
+        hidDevice.write(utils)
+        time.sleep(INTERVAL)
 except IOError as ex:
-   print(ex)
-   print("Failed to open device for writing. Either you are using the wrong device (incorrect VENDOR_ID/PRODUCT_ID), or you need superuser rights.")
+    print(ex)
+    print("Failed to open device for writing. Either you are using the wrong device (incorrect VENDOR_ID/PRODUCT_ID), "
+          "or you need superuser rights.")
+except KeyboardInterrupt:
+    print("\nScript terminated by user.")
+finally:
+    if 'hidDevice' in locals():
+        hidDevice.close()
