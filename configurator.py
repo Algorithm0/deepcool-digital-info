a1f1316be7bd957eb0222df2310124b9ec57996e
Sergey Vorontsov <aurum1997@gmail.com>
2024-05-04 17:43:20 +0300

    Configurator update
    - Support for new device file format;
    - Checking the format of the user device configuration file;
    - Ability to configure video card usage;
    - Ability to customize the display mode for complex devices

--- configurator.py
+++ configurator.py
@@ -3,6 +3,7 @@ import stat
 import subprocess
 from subprocess import Popen
 import click
+import deepcool
 
 print("Hello! This script should help you configure the program.")
 print("It is recommended to run the script with superuser rights to ensure that he has access to the necessary "
@@ -93,46 +94,63 @@ def selectNum(maximum: int, text, default: int = None):
 print("All modules are installed! Great! Well, let's now try to identify your device!")
 json_devices_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'user-devices.json5'))
 reedCustom = os.path.isfile(json_devices_file)
+json_devices = None
 if reedCustom:
     reedCustom = click.confirm("It seems that you have already visited us! A custom device configuration file was "
                                "detected. To use him?", default=True)
 
+if reedCustom:
+    with open(json_devices_file) as f:
+        json_devices = json5.load(f)
+
+    if "CH510" not in json_devices:
+        print("Oops. It was not possible to read the old configuration. It's probably because the developer changed "
+              "the file format... Sorry. Let's set everything up first!")
+        reedCustom = False
+
 if not reedCustom:
     json_devices_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'devices.json5'))
+    with open(json_devices_file) as f:
+        json_devices = json5.load(f)
 
 index = 1
-with open(json_devices_file) as f:
-    json_devices = json5.load(f)
-    try:
-        for device in json_devices["devices"]:
-            print(index, device["name"])
-            index += 1
-    except KeyError:
-        print("Device configuration file has wrong format")
-        exit(1)
+try:
+    for device in json_devices:
+        print(index, device)
+        index += 1
+except KeyError:
+    print("Device configuration file has wrong format")
+    exit(1)
 print(index, "No my device")
 print("0 I think I forgot to turn off the iron! (Go out)")
 
 selected = selectNum(index, 'Please select')
 
 TARGET_ARGS = []
+userDeviceName = ""
 
 base_script = os.path.abspath(os.path.join(os.path.dirname(__file__), 'deepcool.py'))
 check = False
 if selected != index:
+    count_index = 1
+    for device in json_devices:
+        if selected == count_index:
+            userDeviceName = device
+            break
+        count_index += 1
+
     running_procs = Popen(
-        [base_script, "-j", json_devices_file, "-d", f"{json_devices['devices'][selected - 1]['name']}", "-t"])
+        [base_script, "-j", json_devices_file, "-d", f"{userDeviceName}", "-t"])
     check = click.confirm("Wait a little and see what's wrong with your device. Do you see that it shows some random "
                           "data?", default=True)
     running_procs.kill()
     if check:
         print("Hooray! I congratulate you! The worst is over! Let's now make the information meaningful!")
         TARGET_ARGS.append("-d")
-        TARGET_ARGS.append(json_devices["devices"][selected - 1]["name"])
+        TARGET_ARGS.append(userDeviceName)
     else:
         print("It's a pity... It's really strange. But let's try to move on to manual configuration")
-
-userDeviceName = ""
+        userDeviceName = ""
 
 
 def tryDevice():
@@ -174,14 +192,15 @@ def tryDevice():
                 "Please enter the model number of your device (similar to CH510, AK400). If "
                 "your device was already on the list, then add some prefix (AK400-2)",
                 type=str)
-            json_devices["devices"].append({'name': newUserDeviceName,
-                                            'vendor_id': device_list[selectedHid - 1]['vendor_id'],
-                                            'product_id': device_list[selectedHid - 1]['product_id'],
-                                            'simple': (selectedMode == 2)})
+            json_devices[newUserDeviceName] = {
+                'vendor_id': device_list[selectedHid - 1]['vendor_id'],
+                'product_id': device_list[selectedHid - 1]['product_id'],
+                'simple': (selectedMode == 2)
+            }
             new_json = os.path.abspath(os.path.join(os.path.dirname(__file__), 'user-devices.json5'))
             with open(new_json, "w") as write:
                 json5.dump(json_devices, write, sort_keys=True, indent=4)
-            os.chmod(new_json, stat.S_IRWXO)
+            os.chmod(new_json, stat.S_IROTH | stat.S_IWOTH)
             return newUserDeviceName
         else:
             print("It's a pity... It's really strange. But let's try to move on to manual configuration")
@@ -197,11 +216,9 @@ if selected == index or not check:
         exit(1)
     TARGET_ARGS.clear()
     TARGET_ARGS.append("-d")
-    TARGET_ARGS.append(tryDevice())
+    userDeviceName = tryDevice()
+    TARGET_ARGS.append(userDeviceName)
     reedCustom = True
-
-print("And so, then everything should be simpler. We will look at the temperature sensors that we were able to detect "
-      "and select something suitable.")
 
 
 def showSensors():
@@ -218,41 +235,81 @@ def showSensors():
     return index_sensor
 
 
-selected = 0
-while True:
-    print()
-    index = showSensors() + 1
-    print()
-    print(f"{index}     Update")
-    print("0     Exit from configurator")
-    selected = selectNum(index, 'Please select variant')
-    if selected != index:
-        break
+print("And so, then everything should be simpler.")
+print()
+print("1 GPU (supported exclusively by Nvidia on proprietary driver)")
+print("2 CPU (sensor selection)")
+print("0 Exit from configurator")
+selected = selectNum(maximum=2, default=2, text='Select which device information you want to display')
 
+if selected == 1:
+    print("Well, let's try to see if we can see what's wrong with your video card.")
+    gpu_temp = deepcool.get_gpu_temperature(deepcool.GpuVendor.nvidia_prop)
+    gpu_usage = deepcool.get_gpu_usage(deepcool.GpuVendor.nvidia_prop)
+    print(f"GPU temperature: {gpu_temp} °C")
+    print(f"GPU usage:       {gpu_usage} %")
+    print("If you see zeros instead of values and errors appear in the output, then this is not good - we are "
+          "unlikely to be able to configure everything with your video card.")
+    print("1 Everything seems to be ok! I will use GPU")
+    print("2 Go to CPU settings")
+    print("0 Exit from configurator")
+    selected = selectNum(maximum=2, default=1, text='Select')
+    if selected == 1:
+        TARGET_ARGS.append("-g")
 
-selected -= 1
-index = 0
-sensor = ""
-sensor_index = 0
-for base in sensors:
-    index += len(sensors[base])
-    if index > selected:
-        sensor = base
-        sensor_index = selected - (index - len(sensors[base]))
-        break
+if selected == 2:
+    print("We will look at the temperature sensors that we were able to detect and select something suitable.")
 
-print("Selected sensor: ", sensor)
-print(sensors[sensor][sensor_index])
+    selected = 0
+    while True:
+        print()
+        index = showSensors() + 1
+        print()
+        print(f"{index}     Update")
+        print("0     Exit from configurator")
+        selected = selectNum(index, 'Please select variant')
+        if selected != index:
+            break
 
-TARGET_ARGS.append("-s")
-TARGET_ARGS.append(sensor)
-TARGET_ARGS.append("-z")
-TARGET_ARGS.append(f"{sensor_index}")
+    selected -= 1
+    index = 0
+    sensor = ""
+    sensor_index = 0
+    for base in sensors:
+        index += len(sensors[base])
+        if index > selected:
+            sensor = base
+            sensor_index = selected - (index - len(sensors[base]))
+            break
+
+    print("Selected sensor: ", sensor)
+    print(sensors[sensor][sensor_index])
+
+    TARGET_ARGS.append("-s")
+    TARGET_ARGS.append(sensor)
+    TARGET_ARGS.append("-z")
+    TARGET_ARGS.append(f"{sensor_index}")
 
 TARGET_ARGS.append("-i")
 interval = selectNum(60, 'Now enter the data output interval in seconds '
                          '(maximum - 60, 0 - to exit the configurator)', 2)
 TARGET_ARGS.append(f"{interval}")
+
+if not json_devices[userDeviceName]['simple']:
+    print("1 TEMP -> COMPLEX INTERVAL -> USAGE -> INTERVAL")
+    print("2 TEMP -> INTERVAL")
+    print("3 USAGE -> INTERVAL")
+    print("0 Exit from configurator")
+    complex_mode = selectNum(maximum=3, default=1, text='Please select the information display cycle for your device')
+    if complex_mode == 2:
+        TARGET_ARGS.append('--disable-usage')
+    elif complex_mode == 3:
+        TARGET_ARGS.append('--disable-temp')
+    else:
+        TARGET_ARGS.append("--complex-interval")
+        complex_interval = selectNum(60, 'Now enter the data output interval in seconds '
+                                         '(maximum - 60, 0 - to exit the configurator)', interval)
+        TARGET_ARGS.append(f"{complex_interval}")
 
 if reedCustom:
     TARGET_ARGS.append("-u")
@@ -272,7 +329,6 @@ if not click.confirm("Now let's run the installation script. Launch?", default=T
     print()
     print("Bye!")
     exit(0)
-
 
 install_script = os.path.abspath(os.path.join(os.path.dirname(__file__), 'install.sh'))
 TARGET_ARGS.insert(0, install_script)
